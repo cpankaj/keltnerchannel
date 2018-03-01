4d06c5721626fc11c6d5475a873dafbf40281969
Pankaj C <pankaj17n@outlook.com>
2018-03-01 14:56:28 +0530

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "extends": "google"
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-'use strict'
+'use strict';
 
+/**
+ * Prints hello - a dummy function
+ */
 export function printHello() {
-    console.log('hello')
+    console.log('hello');
 }
 
+/**
+ * Prints world - a dummy function
+ */
 export function printWorld() {
-    console.log('world')
+    console.log('world');
 }
